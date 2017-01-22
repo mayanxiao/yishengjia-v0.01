6bd87f9dcd59aaef14ac737557992146d1d0cdd9
mayanxiao <yutouleetown@gmail.com>
2017-01-22 09:52:13 +0800

    yishengjia v0.01

---
+++ yishengjia/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/wholelife-banner.jpg" >
+    <img src="./assets/wholelife.jpg" v-if="portable > 1000">
+    <img src="./assets/wholelife-portable.jpg" v-if="portable <= 1000">
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      portable: document.body.clientWidth
+    }
+  },
+  components: {
+
+  },
+  watch: {
+    portable: function() {
+      return document.body.clientWidth
+    }
+  }
+}
+</script>
+
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  width: 100%;
+  img {
+    width: 100%;
+  }
+}
+</style>
